218cb73b5ae3523554594d5922ef4382905484e5
Gaurav Singh Sirari <gauravsirari424@gmail.com>
2025-06-10 20:31:57 +0530

    part1 complete

---
+++ part1.js
@@ -0,0 +1,17 @@
+let name = "Gaurav"
+// console.log(name.length);
+
+let firstName = "gaurav";
+console.log(firstName.length);
+console.log(firstName[firstName.length-1]);
+
+
+let example = "apnacollege"+123;
+console.log(example);
+
+let example1  = " ";
+console.log(example1.length);
+
+
+
+
